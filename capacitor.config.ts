a17e3fadefb38675243184f3ceb113e97c3199d7
Siva Pallem <siva.pallem@rabobank.com>
2025-07-26 18:36:17 +0530

    Android
    Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.chat.app',
-  appName: 'ChattingApplication',
+  appName: 'ChatApplication',
   webDir: 'build'
 };
 
