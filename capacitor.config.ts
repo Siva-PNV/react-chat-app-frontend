07322ee657af4446f9f388c3095a42d4a5b05e10
Siva Pallem <siva.pallem@rabobank.com>
2025-07-26 18:26:58 +0530

    android app
    android app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.chat.app',
+  appName: 'ChattingApplication',
+  webDir: 'build'
+};
+
+export default config;
